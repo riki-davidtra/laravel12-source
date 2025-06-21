31bf088be36e674c5d69ca9982a05d4befe79469
Jaman IT <rikidavidtra.2310@gmail.com>
2025-06-21 15:20:08 +0700

    update

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -28,35 +28,21 @@ $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(valu
 sidebarToggle = JSON.parse(localStorage.getItem('sidebarToggle')) ?? false;
 $watch('sidebarToggle', value => localStorage.setItem('sidebarToggle', JSON.stringify(value)));" :class="{ 'dark bg-gray-900': darkMode === true }">
 
-    <!-- ===== Preloader Start ===== -->
     @include('dashboard.partials.preloader')
-    <!-- ===== Preloader End ===== -->
 
-    <!-- ===== Page Wrapper Start ===== -->
     <div class="flex h-screen overflow-hidden">
-        <!-- ===== Sidebar Start ===== -->
         @include('dashboard.partials.sidebar')
-        <!-- ===== Sidebar End ===== -->
 
-        <!-- ===== Content Area Start ===== -->
         <div class="relative flex flex-col flex-1 overflow-x-hidden overflow-y-auto">
-            <!-- Small Device Overlay Start -->
             @include('dashboard.partials.overlay')
-            <!-- Small Device Overlay End -->
 
-            <!-- ===== Header Start ===== -->
             @include('dashboard.partials.header')
-            <!-- ===== Header End ===== -->
 
-            <!-- ===== Main Content Start ===== -->
             <main>
                 @yield('content')
             </main>
-            <!-- ===== Main Content End ===== -->
         </div>
-        <!-- ===== Content Area End ===== -->
     </div>
-    <!-- ===== Page Wrapper End ===== -->
 
     <script src="{{ asset('/') }}assets/jquery/jquery-3.6.4.min.js"></script>
     <script src="{{ asset('assets/select2/select2.min.js') }}"></script>
